45244cc7bf1e340086dccd70bcc6f370d5ce8fd9
Pablo Collado Soto <pcolladosoto@gmx.com>
2023-07-14 12:11:20 +0200

    Add library user interface
    Instead of being exposed to the 'nitty-gritty', we have
    defined functions for generating marshalled messages and
    for unmarshalling any received APDU so that the use of the
    library is as easy as posible.

---
+++ encoding.go
@@ -0,0 +1,79 @@
+package bacnet
+
+import (
+	"github.com/ulbios/bacnet/plumbing"
+	"github.com/ulbios/bacnet/services"
+)
+
+const (
+	DEFAULT_ACCEPTED_SIZE        = 1024
+	DEFAULT_SEGMENTATION_SUPPORT = 0x3 // No segmentation
+)
+
+func NewWhois() ([]byte, error) {
+	bvlc := plumbing.NewBVLC(plumbing.BVLCFuncBroadcast)
+	npdu := plumbing.NewNPDU(false, false, false, false)
+	u := services.NewUnconfirmedWhoIs(bvlc, npdu)
+	return u.MarshalBinary()
+}
+
+func NewIAm(deviceId uint32, vendorId uint16) ([]byte, error) {
+	bvlc := plumbing.NewBVLC(plumbing.BVLCFuncBroadcast)
+
+	npdu := plumbing.NewNPDU(false, true, false, false)
+	npdu.DNET = 0xFFFF
+	npdu.DLEN = 0
+	npdu.Hop = 0xFF
+
+	u := services.NewUnconfirmedIAm(bvlc, npdu)
+
+	u.APDU.Objects = services.IAmObjects(deviceId,
+		DEFAULT_ACCEPTED_SIZE, DEFAULT_SEGMENTATION_SUPPORT, vendorId)
+	u.SetLength()
+
+	return u.MarshalBinary()
+}
+
+func NewCACK(service uint8, objectType uint16, instN uint32, propertyId uint8, value float32) ([]byte, error) {
+	bvlc := plumbing.NewBVLC(plumbing.BVLCFuncUnicast)
+	npdu := plumbing.NewNPDU(false, false, false, false)
+
+	c := services.NewComplexACK(bvlc, npdu)
+
+	c.APDU.Service = service
+	c.APDU.InvokeID = 1
+	c.APDU.Objects = services.ComplexACKObjects(objectType, instN, propertyId, value)
+
+	c.SetLength()
+
+	return c.MarshalBinary()
+}
+
+func NewSACK(service uint8) ([]byte, error) {
+	bvlc := plumbing.NewBVLC(plumbing.BVLCFuncUnicast)
+	npdu := plumbing.NewNPDU(false, false, false, false)
+
+	s := services.NewSimpleACK(bvlc, npdu)
+
+	s.APDU.Service = service
+	s.APDU.InvokeID = 1
+
+	s.SetLength()
+
+	return s.MarshalBinary()
+}
+
+func NewError(service, errorClass, errorCode uint8) ([]byte, error) {
+	bvlc := plumbing.NewBVLC(plumbing.BVLCFuncUnicast)
+	npdu := plumbing.NewNPDU(false, false, false, false)
+
+	e := services.NewError(bvlc, npdu)
+
+	e.APDU.Service = service
+	e.APDU.InvokeID = 1
+	e.APDU.Objects = services.ErrorObjects(errorClass, errorCode)
+
+	e.SetLength()
+
+	return e.MarshalBinary()
+}
---
+++ parsing.go
@@ -0,0 +1,69 @@
+package bacnet
+
+import (
+	"github.com/ulbios/bacnet/common"
+	"github.com/ulbios/bacnet/plumbing"
+	"github.com/ulbios/bacnet/services"
+)
+
+const bacnetLenMin = 8
+
+func combine(t, s uint8) uint16 {
+	return uint16(t)<<8 | uint16(s)
+}
+
+// Parse decodes the given bytes.
+func Parse(b []byte) (plumbing.BACnet, error) {
+	if len(b) < bacnetLenMin {
+		return nil, common.ErrTooShortToParse
+	}
+
+	var bvlc plumbing.BVLC
+	var npdu plumbing.NPDU
+	var bacnet plumbing.BACnet
+
+	offset := 0
+
+	if err := bvlc.UnmarshalBinary(b); err != nil {
+		return nil, err
+	}
+	offset += bvlc.MarshalLen()
+
+	if err := npdu.UnmarshalBinary(b[offset:]); err != nil {
+		return nil, err
+	}
+	offset += npdu.MarshalLen()
+
+	var c uint16
+	switch b[offset] >> 4 & 0xFF {
+	case plumbing.UnConfirmedReq:
+		c = combine(b[offset], b[offset+1])
+	case plumbing.ConfirmedReq:
+		c = combine(b[offset], b[offset+3]) // We need to skip the PDU flags and the InvokeID
+	case plumbing.ComplexAck, plumbing.SimpleAck, plumbing.Error:
+		c = combine(b[offset], 0) // We need to skip the PDU flags and the InvokeID
+	}
+
+	switch c {
+	case combine(plumbing.UnConfirmedReq<<4, services.ServiceUnconfirmedWhoIs):
+		bacnet = services.NewUnconfirmedWhoIs(&bvlc, &npdu)
+	case combine(plumbing.UnConfirmedReq<<4, services.ServiceUnconfirmedIAm):
+		bacnet = services.NewUnconfirmedIAm(&bvlc, &npdu)
+	case combine(plumbing.ConfirmedReq<<4, services.ServiceConfirmedReadProperty):
+		bacnet = services.NewConfirmedReadProperty(&bvlc, &npdu)
+	case combine(plumbing.ComplexAck<<4, 0):
+		bacnet = services.NewComplexACK(&bvlc, &npdu)
+	case combine(plumbing.SimpleAck<<4, 0):
+		bacnet = services.NewSimpleACK(&bvlc, &npdu)
+	case combine(plumbing.Error<<4, 0):
+		bacnet = services.NewError(&bvlc, &npdu)
+	default:
+		return nil, common.ErrNotImplemented
+	}
+
+	if err := bacnet.UnmarshalBinary(b); err != nil {
+		return nil, err
+	}
+
+	return bacnet, nil
+}
